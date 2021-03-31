5f5a43a6815138f2b0a4970f22957663bf49586b
AJBayliss81 <aj.bayliss@live.co.uk>
2021-03-31 10:47:24 +0000

    Added products app and wired up. Added products to database

---
+++ products/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+
+def all_products(request):
+    """
+    A view to display all of the products from the database, including sorting
+    and search queries
+    """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    A view to display individual product details
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
