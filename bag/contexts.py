8d8271ffd0a6bc3686d8ec347e8a2d4a75e2eafa
AJBayliss81 <aj.bayliss@live.co.uk>
2021-12-29 16:41:13 +0000

    Added more comments to html and fixed on hover functionality for butons and dropdown menus

--- bag/contexts.py
+++ bag/contexts.py
@@ -6,7 +6,6 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
-    free_delivery_threshold = settings.FREE_DELIVERY_THRESHOLD
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
@@ -23,7 +22,7 @@ def bag_contents(request):
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': free_delivery_threshold,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
 
